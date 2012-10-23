c79044d0491ea8cb83ec16aa356554cce44198fa
sirprize <hoegl@backdoor.ch>
2012-10-23 22:42:48 +0200

    Fix package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ var profile = (function () {
             "dobolo/package": 1
         },
         amdExcludes = {},
-        isTestRe = /\/test\//
+        isTestRe = /\/tests\//
     ;
 
     return {
