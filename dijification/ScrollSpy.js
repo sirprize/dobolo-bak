eb9c8ae61d0e41ec5ffcb42e363bd7980c51145c
sirprize <hoegl@backdoor.ch>
2012-10-23 12:06:00 +0200

    Start dijified ScrollSpy

---
+++ dijification/ScrollSpy.js
@@ -0,0 +1,50 @@
+define([
+    "dojo/_base/declare",
+    "bootstrap/ScrollTopSpyHelper",
+    "dojo/_base/window",
+    "dojo/dom-class",
+    "dojo/dom-attr",
+    "dojo/query",
+    "dojo/on"
+], function (
+    declare,
+    ScrollTopSpyHelper,
+    baseWin,
+    domClass,
+    domAttr,
+    query,
+    on
+) {
+    return declare([], {
+        
+        helper: null,
+        
+        constructor: function (props, scrollingNode) {
+            var props = props || {},
+                scrollingNode = (!scrollingNode || scrollingNode && scrollingNode.tagName === 'BODY') ? baseWin.doc : scrollingNode,
+                offsetNodes = (props.offsetNodeSelector) ? query(props.offsetNodeSelector, scrollingNode) : [],
+                offsetTop = props.offsetTop || 0,
+                wait = props.wait || 100,
+                targetSelector = (props.targetSelector) ? props.targetSelector : null;
+            
+            this.helper = new ScrollTopSpyHelper(scrollingNode, offsetNodes, offsetTop, wait);
+            
+            if (targetSelector) {
+                this.helper.on('active', function (ev) {
+                    // find list items in target
+                    query(targetSelector + ' li').forEach(function (listItem) {
+                        // find anchors in list item
+                        query('> a', listItem).forEach(function (target) {
+                            var href = (domAttr.get(target, 'href') || '').replace(/^#/, '');
+                            domClass[(href === ev.node.id) ? 'add' : 'remove'](listItem, 'active');
+                        });
+                    });
+                });
+            }
+        },
+        
+        destroy: function () {
+            this.helper.destroy();
+        }
+    });
+});
