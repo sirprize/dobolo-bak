fde00af06928f6cbdf8ece94422406849a8141da
sirprize <hoegl@backdoor.ch>
2012-10-23 14:26:39 +0200

    Remove event hanle upon destruction

--- dijification/ScrollSpy.js
+++ dijification/ScrollSpy.js
@@ -18,6 +18,7 @@ define([
     return declare([], {
         
         helper: null,
+        handle: null,
         
         constructor: function (props, scrollingNode) {
             var props = props || {},
@@ -29,22 +30,24 @@ define([
             
             this.helper = new ScrollTopSpyHelper(scrollingNode, offsetNodes, offsetTop, wait);
             
-            if (targetSelector) {
-                this.helper.on('active', function (ev) {
-                    // find list items in target
-                    query(targetSelector + ' li').forEach(function (listItem) {
-                        // find anchors in list item
-                        query('> a', listItem).forEach(function (target) {
-                            var href = (domAttr.get(target, 'href') || '').replace(/^#/, '');
-                            domClass[(href === ev.node.id) ? 'add' : 'remove'](listItem, 'active');
-                        });
+            this.handle = this.helper.on('active', function (ev) {
+                // find list items in target
+                query(targetSelector + ' li').forEach(function (listItem) {
+                    // find anchors in list item
+                    query('> a', listItem).forEach(function (target) {
+                        var href = (domAttr.get(target, 'href') || '').replace(/^#/, '');
+                        domClass[(href === ev.node.id) ? 'add' : 'remove'](listItem, 'active');
                     });
                 });
-            }
+            });
         },
         
         destroy: function () {
             this.helper.destroy();
+            
+            if (this.handle && this.handle.remove) {
+                this.handle.remove();
+            }
         }
     });
 });
